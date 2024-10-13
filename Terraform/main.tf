60ed921da0ba8a462d7fe29839b8361a2fd8ded5
Mohamed Essam <Mohamed.Hasan.34@h-eng.helwan.edu.eg>
2024-10-13 22:35:05 +0300

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -42,7 +42,7 @@ resource "aws_route_table_association" "public_subnet_assoc" {
   route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_security_group" "allow_ssh_http" {
+resource "aws_security_group" "kubernetes" {
   vpc_id = aws_vpc.main_vpc.id
 
   ingress {
@@ -59,6 +59,13 @@ resource "aws_security_group" "allow_ssh_http" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -67,7 +74,36 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 
   tags = {
-    Name = "allow_ssh_http"
+    Name = "kubernetes"
+  }
+}
+
+resource "aws_security_group" "jenkins" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jenkins"
   }
 }
 
@@ -116,13 +152,13 @@ resource "aws_ecr_repository" "my_ecr_repo" {
   }
 }
 
-data "aws_ami" "amazon_linux_2" {
+data "aws_ami" "ubuntu22" {
   most_recent = true
   owners      = ["amazon"] 
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-*"]
   }
 
   filter {
@@ -142,22 +178,31 @@ data "aws_ami" "amazon_linux_2" {
 }
 
 resource "aws_instance" "kubernetes" {
-  ami           = data.aws_ami.amazon_linux_2.id 
+  ami           = data.aws_ami.ubuntu22.id 
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+  vpc_security_group_ids = [aws_security_group.kubernetes.id]
   key_name = "project"  
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
 
   user_data = <<-EOF
               #!/bin/bash
+              apt update
+
+              apt install -y curl unzip
+              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+              unzip awscliv2.zip
+              ./aws/install
+
               curl -sfL https://get.k3s.io | sh -
               # Ensure kubeconfig permissions are set
-              mkdir -p /home/ec2-user/.kube
-              cp /etc/rancher/k3s/k3s.yaml /home/ec2-user/.kube/config
-              chown ec2-user:ec2-user /home/ec2-user/.kube/config
-              chmod 600 /home/ec2-user/.kube/config
-              echo "export KUBECONFIG=/home/ec2-user/.kube/config" >> /home/ec2-user/.bashrc    
+              mkdir -p /home/ubuntu/.kube
+              cp /etc/rancher/k3s/k3s.yaml /home/ubuntu/.kube/config
+              chown ubuntu:ubuntu /home/ubuntu/.kube/config
+              chmod 600 /home/ubuntu/.kube/config
+              echo "export KUBECONFIG=/home/ubuntu/.kube/config" >> /home/ubuntu/.bashrc  
+              
+              kubectl create secret docker-registry ecr-secret --docker-server=975050176026.dkr.ecr.us-east-1.amazonaws.com --docker-username=AWS --docker-password=$(aws ecr get-login-password --region us-east-1) --docker-email=your-email@example.com  
               EOF
 
   tags = {
@@ -166,10 +211,10 @@ resource "aws_instance" "kubernetes" {
 }
 
 resource "aws_instance" "jenkins" {
-  ami           = data.aws_ami.amazon_linux_2.id 
+  ami           = data.aws_ami.ubuntu22.id 
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+  vpc_security_group_ids = [aws_security_group.jenkins.id]
   key_name = "project" 
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
 
